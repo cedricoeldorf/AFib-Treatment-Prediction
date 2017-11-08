a6f1850f1b7788df9c292e149ea25477a8c79fba
cedric <cedric.oeldorf@yahoo.de>
2017-11-08 19:29:58 +0200

    started convNN training script, calls laod data form load_data script

---
+++ convolutionalNN.py
@@ -0,0 +1,2 @@
+from load_data import load_data
+X, y = load_data()
--- load_data.py
+++ load_data.py
@@ -2,7 +2,9 @@ import numpy as np
 import pickle
 import pandas as pd
 
-with open ('../data/training/x.pkl', 'rb') as fp:
-    X = pickle.load(fp)
+def load_data():
+    with open ('../data/training/x.pkl', 'rb') as fp:
+        X = pickle.load(fp)
 
-y = pd.read_excel('../data/training/y.xlsx')
+    y = pd.read_excel('../data/training/y.xlsx')
+    return X, y
