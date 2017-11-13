fixed data merge, merged demographics, convolutional code working but computation heavy

convolutionalNN.py
@@ -3,15 +3,23 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-
+from sklearn.decomposition import TruncatedSVD
 from load_data import load_data
-X, y = load_data()
+import numpy as np
+X, y = load_data(with_demographics = True,from_source = True)
 
 batch_size = 128
 num_classes = 2
 epochs = 2
 
-img_rows, img_cols = 2500, 12
+img_rows, img_cols = 2500, 55
+
+## Create training and testing set
+#########################
+## TO DO:
+## Cross validation
+## Dimensionality reduction to avoid exhaustion of machine resources
+#########################
 
 x_train = X[0:290]
 x_test = X[290:]
@@ -28,5 +36,45 @@ x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
+
+## One hot encode target
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+
+"""
+###########
+## dimensionality reduction
+##########
+x_train = [np.matrix(xi) for xi in x_train]
+x_test = [np.matrix(xi) for xi in x_test]
+pca = TruncatedSVD(n_components=10).fit(x_train)
+x_train = pca.transform(X_train)
+x_test = pca.transform(X_test)
+"""
+############
+## Construct model
+#############
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])

load_data.py
@@ -2,9 +2,45 @@ import numpy as np
 import pickle
 import pandas as pd
 
-def load_data():
+def load_data(with_demographics = True, from_source = True):
+    if (from_source == True) & (with_demographics == True):
+        with open ('../data/training/X.pkl', 'rb') as fp:
+            X = pickle.load(fp)
+    if (from_source == False) & (with_demographics == True):
+        X = prep_data()
+        with open('../data/training/X.pkl', 'wb') as fp:
+            pickle.dump(X, fp)
+    if (from_source == True) & (with_demographics == False):
+        with open ('../data/training/x.pkl', 'rb') as fp:
+            X = pickle.load(fp)
+            #X = np.delete(X, [76,151,245])
+
+    X = np.array(X)
+    y = pd.read_excel('../data/training/y.xlsx')
+    y = y.drop(y.index[[76,151,245]]).reset_index()
+    return X, y
+
+
+def prep_data():
     with open ('../data/training/x.pkl', 'rb') as fp:
         X = pickle.load(fp)
+    demog = pd.read_excel('../data/demographics.xls')
 
-    y = pd.read_excel('../data/training/y.xlsx')
-    return X, y
+    ## PATIENTS THAT HAD TO BE DROPPED
+    ## manually removed from ecg to retain 3 dimensional shape
+    #X = np.delete(X, [76,151,245])
+    demog = demog.drop(demog.index[[76,151,245]]).reset_index()
+
+    ## missing cells, interpolate
+    demog = demog.interpolate()
+
+    demog_matrix = []
+    for i in range(0,len(demog)):
+        print("Patient " + str(i) + ' out of ' + str(len(demog)))
+        patient = pd.DataFrame()
+        patient = patient.append([demog.loc[i]]*2500,ignore_index=True)
+        patient =  np.asarray(patient)
+        #demog_matrix.append(patient)
+        X[i] = np.hstack((patient,X[i]))
+
+    return X

merge_data.py
@@ -25,10 +25,12 @@ x = []
 for num in names:
     print(num)
     mat_contents = sio.loadmat('./patient_training' + num + '.mat')
-    mat_contents = np.array(mat_contents['ecg'])
+    mat_contents = mat_contents['ecg']
     x.append(mat_contents)
 
-x = np.array([np.array(xi) for xi in x])
+#x = np.array([np.array(xi) for xi in x])
+#x = [xi for xi in x]
+#x = np.ndarray(x)
 ## write list of matrices to pickle
 with open('x.pkl', 'wb') as fp:
     pickle.dump(x, fp)